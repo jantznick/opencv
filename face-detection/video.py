09ec765f9a318c2b80981d28bffc98f097b350dc
Nick Jantz <thenickjantz@gmail.com>
2020-10-15 22:30:57 -0500

    add video analysis

---
+++ face-detection/video.py
@@ -0,0 +1,91 @@
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", default='deploy.prototxt.txt',
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", default='model.caffemodel',
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-v", "--video", required=True,
+	help="path to input video file")
+args = vars(ap.parse_args())
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+
+cap = cv2.VideoCapture(args["video"])
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+print(fps)
+print(width)
+print(height)
+
+ret, frame = cap.read()
+frame = imutils.resize(frame, width=400)
+
+# grab the frame dimensions and convert it to a blob
+(h, w) = frame.shape[:2]
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('output.mov',fourcc, int(fps), (int(w), int(h)))
+
+while(cap.isOpened()):
+	ret, frame = cap.read()
+
+	frame = imutils.resize(frame, width=400)
+
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+		(300, 300), (104.0, 177.0, 123.0))
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the
+		# prediction
+		confidence = detections[0, 0, i, 2]
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence < args["confidence"]:
+			continue
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+
+		# draw the bounding box of the face along with the associated
+		# probability
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(frame, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(frame, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+	# cv2.imshow('frame', frame)
+	out.write(frame)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
