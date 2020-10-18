a14fde48a244d8897f99b08153f6888729bf9769
Nick Jantz <thenickjantz@gmail.com>
2020-10-18 15:35:02 -0500

    license plate finding works

--- license-plate-recognition/main.py
+++ license-plate-recognition/main.py
@@ -5,7 +5,7 @@ import imutils
 # import re
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", default="sample-images/2.PNG",
+ap.add_argument("-i", "--image", default="sample-images/4.PNG",
 	help="path to input image")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
@@ -14,14 +14,13 @@ args = vars(ap.parse_args())
 img = cv2.imread(args["image"])
 img = cv2.resize(img, (480,620) )
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
-edged = cv2.Canny(gray, 150, 250) #Perform Edge detection
 
-ret,thresh1 = cv2.threshold(gray,125,255,cv2.THRESH_BINARY)
+gray = cv2.bilateralFilter(gray, 13, 50, 50)
+edged = cv2.Canny(gray, 10, 150) #Perform Edge detection
 
-contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(edged.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = sorted(contours,key=cv2.contourArea, reverse = True)[:10]
 screenCnt = None
-
 
 for c in contours:
 	# approximate the contour
@@ -36,12 +35,17 @@ for c in contours:
 img_contours = np.zeros(img.shape)
 cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
 
-# cv2.imshow('threshold',thresh1)
+img_contour = np.zeros(img.shape)
+if screenCnt.any():
+	cv2.drawContours(img_contour, [screenCnt], -1, (0,255,0), 3)
+
+cv2.imshow('img',img)
 # cv2.waitKey(0)
 # cv2.imshow('contours', img_contours)
 # cv2.waitKey(0)
-# cv2.imshow('grey', gray)
+if screenCnt.any():
+	cv2.imshow('contour', img_contour)
 # cv2.waitKey(0)
-cv2.imshow('edged', edged)
+# cv2.imshow('edged', edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
