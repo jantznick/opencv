a410db4454094d938e3ca72a42a1353496302f78
Nick Jantz <thenickjantz@gmail.com>
2020-11-07 22:18:38 -0600

    add default to face detection

--- face-detection/main.py
+++ face-detection/main.py
@@ -7,9 +7,9 @@ import re
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
-ap.add_argument("-p", "--prototxt", required=True,
+ap.add_argument("-p", "--prototxt", default='deploy.prototxt.txt',
 	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
+ap.add_argument("-m", "--model", default='model.caffemodel',
 	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
